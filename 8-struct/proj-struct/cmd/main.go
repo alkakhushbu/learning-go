0e2d42ea9bd4d9ecd445c8d2d1cff5f941a9a5d9
alkakhushbu <alkakhushbu@gmail.com>
2024-11-28 12:40:28 +0530

    First commit

---
+++ 8-struct/proj-struct/cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"proj-struct/database"
+	"proj-struct/models"
+)
+
+type user struct {
+	name string
+}
+
+func main() {
+
+	// c.db = "mysql" // not allowed, db is not exported
+	c := database.NewConf("mysql")
+	fmt.Println(c)
+	c.Ping()
+
+	//u := new(user)
+	//u1 := &user{}
+	models.CreateUser("ajay", c)
+
+}
